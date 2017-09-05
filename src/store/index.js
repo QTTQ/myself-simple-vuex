08e4f49c4ed9b5160b5a993bc70b6dd754649763
unknown <Administrator@JHLC03ZJ0039.ad5173.com>
2017-09-05 10:19:03 +0800

    简单标准vuex示例

--- src/store/index.js
+++ src/store/index.js
@@ -6,15 +6,14 @@ Vue.use(Vuex);
 import mutations from './mutations'
 import actions from './actions'
 import getters from './getters'
+import state from './state'
 
-const state = {
-	count: 20
-};
+
+// const state = {
+// 	count: 20
+// };
 
 export default new Vuex.Store({
-	// modules:{
-	// 	mutations
-	// },
 	state,
 	mutations,
 	getters,
